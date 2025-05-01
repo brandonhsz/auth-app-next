a10cf84f1a1ffa9d0eed931b76ff256010192ca9
Brandonhsz <brandonhszzz@gmail.com>
2025-05-01 06:00:11 -0600

    feat: implement SignIn form with validation and submission handling

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,70 @@
+"use client";
+import { useForm } from "@/hooks";
+import { BUTTON_COLORS } from "@/lib/constants";
+import { Button, Card, PasswordInput, TextInput } from "@mantine/core";
+import { LockKeyhole, Mail } from "lucide-react";
+import { useTranslations } from "next-intl";
 import React from "react";
+import { SignInSchema, TSignInSchema } from "@/modules/auth/schemas";
+import { useLoginTenantMutation } from "@/modules/auth/api";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const t = useTranslations("SignIn");
+  const signInMutation = useLoginTenantMutation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    schema: SignInSchema,
+    resetOptions: {
+      keepValues: true,
+    },
+    mode: "all",
+  });
+
+  const onSubmit = async (data: TSignInSchema) => {
+    await signInMutation.mutateAsync(data);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="relative flex gap-4 px-56 flex-col items-center justify-center h-screen"
+    >
+      <Card shadow="lg" className="flex flex-col gap-4">
+        <h2 className="font-bold text-xl">{t("title")}</h2>
+        <p>{t("description")}</p>
+
+        <div className="flex flex-col gap-4">
+          <TextInput
+            {...register("email")}
+            leftSection={<Mail />}
+            label={t("email")}
+            error={errors.email?.message}
+          />
+          <PasswordInput
+            {...register("password")}
+            leftSection={<LockKeyhole />}
+            label={t("password")}
+            error={errors.password?.message}
+          />
+        </div>
+
+        <Button
+          type="submit"
+          disabled={!isValid}
+          color={BUTTON_COLORS.primary}
+          className="mb-4"
+        >
+          {t("sign_in")}
+        </Button>
+      </Card>
+      <Button variant="transparent">
+        <span className="underline">{t("forgot_password")}</span>
+      </Button>
+    </form>
+  );
 };
 
 export default SignIn;

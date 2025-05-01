1b7574ebddcc3e35c37555744f10dc06646274f7
Brandonhsz <brandonhszzz@gmail.com>
2025-05-01 06:42:02 -0600

    feat: add redux-persist for state persistence and update store configuration

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { queryClient } from "@/lib/api/api";
-import { store } from "@/store/store";
+import { store, persistor } from "@/store/store";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -13,7 +14,11 @@ interface ProvidersProps {
 export const Providers = ({ children }: ProvidersProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>{children}</Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          {children}
+        </PersistGate>
+      </Provider>
     </QueryClientProvider>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+  persistReducer,
+} from "redux-persist";
 import { signInReducer } from "@/modules/auth/store";
-export const store = configureStore({
-  reducer: {
-    signInReducer,
-  },
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  signInReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: {
+    persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
